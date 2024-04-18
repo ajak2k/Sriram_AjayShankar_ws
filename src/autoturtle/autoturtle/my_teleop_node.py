84115e81f980950ba9034664dcdf54fb8862c6a3
Ajay <sajaax2000@gmail.com>
2024-04-18 01:27:12 -0700

    changed the code, added new my_teleop_node.py with curses

--- src/autoturtle/autoturtle/my_teleop_node.py
+++ src/autoturtle/autoturtle/my_teleop_node.py
@@ -1,78 +1,78 @@
-import rclpy # type: ignore
-from rclpy.node import Node # type: ignore
-from geometry_msgs.msg import Twist # type: ignore
-import sys
-import termios
-import tty
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import curses
 
-class my_teleop(Node):
+#define our publisher class
+class my_teleop_node(Node):
+    #construstor to set up the publishing method or mention the topic to publish to
     def __init__(self):
         super().__init__('my_teleop_node')
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.run_teleop()
-
-    def get_key(self):
-        # Set up terminal settings for raw input
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        tty.setraw(sys.stdin.fileno())
-        try:
-            key = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return key
-
-    def run_teleop(self):
-        # Display control instructions
-        print("Control the turtle using keyboard:")
-        print("Use 'W' for forward, 'S' for backward, 'A' for left, 'D' for right")
-        print("Press 'Q' to quit")
-
+        #launch the funciton to control the bot
+        self.controller()
+        
+    def controller(self):
+        #initialize curses 
+        stdscr = curses.initscr()
+        curses.def_shell_mode()
+        curses.noecho()
+        curses.cbreak()
+        stdscr.nodelay(True)
+        
         twist = Twist()
 
         while True:
-            key = self.get_key()
-            
-            if key == 'q':
-                print("Exiting teleop...")
-                break
-            elif key == 'w':
-                twist.linear.x = 0.5
+            #Prompt the user
+            stdscr.addstr(0,0,"Please use the WASD keys to control the bot or Q to quit the program", curses.A_BOLD)
+            #need to use try as .getkey() returns an exception when no input is given
+            try:           
+                key = stdscr.getkey()
+            except curses.error: # no input
+                twist.linear.x = 0.0
                 twist.angular.z = 0.0
-            elif key == 's':
-                twist.linear.x = -0.5
-                twist.angular.z = 0.0
-            elif key == 'a':
+                continue
+            if key =='w' or key =='W':
+                twist.linear.x = 1.0
+                twist.angular.z= 0.0
+            elif key == 'a' or key =='A':
                 twist.linear.x = 0.0
                 twist.angular.z = 0.5
-            elif key == 'd':
+            elif key == 's' or key =='S':
+                twist.linear.x = -1.0
+                twist.angular.z = 0.0
+            elif key == 'd' or key == 'D':
                 twist.linear.x = 0.0
                 twist.angular.z = -0.5
+            elif key == 'q' or key == 'Q':
+                    break
             else:
                 twist.linear.x = 0.0
-                twist.angular.z = 0.0
+                twist.angular.z = 0.0          
+            self.publisher_.publish(twist)    
+            stdscr.refresh()
+        #End of while loop
+        #terminate curses
+        curses.nocbreak()
+        curses.echo()
+        curses.reset_shell_mode()
+        curses.endwin()
+    #end of function
+#end of class           
 
-            # Publish the Twist message
-            self.publisher_.publish(twist)
-
-        # Stop the turtle when quitting
-        twist.linear.x = 0.0
-        twist.angular.z = 0.0
-        self.publisher_.publish(twist)
+#Main Function
 
 def main():
-    # Initialize rclpy
+    #start the ros2 client 
     rclpy.init()
-    
-    # Create the teleop node
-    teleop_node = my_teleop()
-
-    # Keep the node running
+    #create the object of the publisher
+    teleop_node = my_teleop_node()
+    #spin it to keep it running
     rclpy.spin(teleop_node)
-
-    # Shutdown rclpy
+    #shut it down
     teleop_node.destroy_node()
     rclpy.shutdown()
-
+    print('Thanks for Playing!')
+#Starting point into the code
 if __name__ == '__main__':
     main()
