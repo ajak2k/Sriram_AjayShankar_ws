8bb7030a0ff481bdf8b646f0a37d79782cac5ad6
ajak2k <sajaax2000@gmail.com>
2024-05-17 16:28:48 -0700

    created rrt_node; updated package and setup

--- src/turtlebot/setup.py
+++ src/turtlebot/setup.py
@@ -4,7 +4,7 @@ package_name = 'turtlebot'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.4.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,13 +15,16 @@ setup(
     zip_safe=True,
     maintainer='ajak2k',
     maintainer_email='sajaax2000@gmail.com',
-    description='A package that impliments PID control for the turtlebot3 in gazebo. The controller gets inputs from the motion_planner',
+    description='A package that impliments PID control and RRT based motion planning for the turtlebot3 in gazebo. The controller gets inputs from the motion_planner',
     license='MIT-0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'pid_controller = turtlebot.pid_controller:main',
-            'motion_planner = turtlebot.motion_planner:main'
+            'motion_planner = turtlebot.motion_planner:main',
+            'map_reader_template = turtlebot.map_reader_template:main',
+            'rrt = turtlebot.rrt:main',
+            'rrt_node = turtlebot.rrt_node:main',
         ],
     },
 )
